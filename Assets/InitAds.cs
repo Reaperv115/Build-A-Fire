6d8abe856615530a5921d62dc32e49cc1a268441
rjs5785 <rjs5785@hotmail.com>
2022-12-21 11:59:04 -0500

    implemented the camera previously and have banner ads loading at the bottom of the screen

---
+++ Assets/InitAds.cs
@@ -0,0 +1,41 @@
+ using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class InitAds : MonoBehaviour, IUnityAdsInitializationListener
+{
+    string AndroidGameId = "5030833";
+    bool testMode;
+
+    string androidadunitId = "Banner_Android";
+    string adunitId;
+    // Start is called before the first frame update
+    void Start()
+    {
+        adunitId = androidadunitId;
+        testMode = true;
+        InitializeAds();
+    }
+
+    void InitializeAds()
+    {
+        var gameId = AndroidGameId;
+        Advertisement.Initialize(gameId, testMode, this);
+        Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Advertisement.Banner.Show(adunitId);
+    }
+
+    public void OnInitializationComplete()
+    {
+        Debug.Log("initialization complete");
+    }
+
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
+    {
+        Debug.Log(message + " " + error);
+    }
+}
