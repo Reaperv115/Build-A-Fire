e019145d1bcd8607303dd44f6e866913dd0cb32c
rjs5785 <rjs5785@hotmail.com>
2022-12-27 17:22:03 -0500

    working on the backyard enviornment. had the camera controls done, but in order to get the camera to not pass through the ground it needs to move via the rigidbody and now that is giving some undesired behavior

--- Assets/MoveCamera.cs
+++ Assets/MoveCamera.cs
@@ -4,20 +4,47 @@ using UnityEngine;
 
 public class MoveCamera : MonoBehaviour
 {
-    float speed = 10f;
+    float speed = 1000f;
+    Rigidbody rb;
     [SerializeField] GameObject firePit;
+    bool isMoving = false;
+    Vector3 touchPosition;
+    Quaternion deltaRotation;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        Debug.Log(isMoving);
+        
+        transform.LookAt(firePit.transform);
+        foreach(Touch touch in Input.touches)
+        {
+            if (touch.phase.Equals(TouchPhase.Moved))
+            {
+                isMoving = true;
+                touchPosition = touch.deltaPosition;
+            }
+            if (touch.phase.Equals(TouchPhase.Ended))
+                isMoving = false;
+                
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        foreach(Touch touch in Input.touches)
-        {
-            transform.Translate(touch.deltaPosition * speed * Time.deltaTime);
-            transform.LookAt(firePit.transform);
-        }
+        deltaRotation = Quaternion.Euler(touchPosition * Time.deltaTime);
+        rb.MoveRotation(deltaRotation);
+        if (isMoving)
+            Move(touchPosition);
+        rb.velocity = Vector3.zero;
+    }
+
+    void Move(Vector3 t)
+    {
+        rb.AddForce(t * speed * Time.deltaTime);
     }
 }
